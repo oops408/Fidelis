10f7b0a892a4a4548685554f09448e4339a8f918
oops408 <68081409+oops408@users.noreply.github.com>
2023-02-24 10:03:52 -0800

    Update setup.py

--- src/board/setup.py
+++ src/board/setup.py
@@ -17,6 +17,7 @@ class ChessBoard:
             ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
             ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']
         ]
+        self.current_turn = 'white'
 
     def print_board(self):
         """
@@ -26,3 +27,34 @@ class ChessBoard:
             print('+---+---+---+---+---+---+---+---+')
             print('| ' + ' | '.join(rank) + ' |')
         print('+---+---+---+---+---+---+---+---+')
+
+    def move(self, start_pos, end_pos):
+        """
+        Moves a piece from the start position to the end position on the board.
+        Returns True if the move is legal and False otherwise.
+        """
+        # Check that start and end positions are valid
+        if not self.is_valid_pos(start_pos) or not self.is_valid_pos(end_pos):
+            return False
+
+        start_row, start_col = start_pos
+        end_row, end_col = end_pos
+
+        # Check that start position contains a piece belonging to the current player
+        if self.board[start_row][start_col] == ' ' or \
+           (self.current_turn == 'white' and self.board[start_row][start_col].islower()) or \
+           (self.current_turn == 'black' and self.board[start_row][start_col].isupper()):
+            return False
+
+        # Check that end position is empty or contains an enemy piece
+        if self.board[end_row][end_col] != ' ' and \
+           ((self.current_turn == 'white' and self.board[end_row][end_col].isupper()) or \
+            (self.current_turn == 'black' and self.board[end_row][end_col].islower())):
+            return False
+
+        # Check that the move is valid for the selected piece
+        if self.board[start_row][start_col] == 'N' or self.board[start_row][start_col] == 'n':
+            valid_moves = [(start_row + 1, start_col + 2), (start_row + 2, start_col + 1),
+                           (start_row + 2, start_col - 1), (start_row + 1, start_col - 2),
+                           (start_row - 1, start_col - 2), (start_row - 2, start_col - 1),
+                           (start_row - 2, start_col + 1
